ebdba66c7771fdbbcbdb180be3cc7629e1a90399
tyler-cranmer <tyler.cranmer@outlook.com>
2022-04-20 15:45:05 -0600

    vault contract

--- contracts/myVault.sol
+++ contracts/myVault.sol
@@ -11,12 +11,12 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol'; //swaps done on uniswap
-import '@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol'; //price quoter oracle
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol"; //swaps done on uniswap
+import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol"; //price quoter oracle
 
 // EACAggregatorProxy is used for chainlink oracle
 interface EACAggregatorProxy {
-    function latestAnswer() external view returns(int256); //gets the price for ethereum.
+    function latestAnswer() external view returns (int256); //gets the price for ethereum.
 }
 
 //Uniswap v3 interface
@@ -29,108 +29,186 @@ interface DepositableERC20 is IERC20 {
     function deposit() external payable; //add deposit interface for WETH
 }
 
-
 contract myVault {
-uint256 public version = 1;
-address public daiAddress;
-address public wethAddress;
-address public uniswapV3QuoterAddress;
-address public uniswapV3RouterAddress;
-address public chainLinkETHUSDAddress;
+    uint256 public version = 1;
+    address public daiAddress;
+    address public wethAddress;
+    address public uniswapV3QuoterAddress;
+    address public uniswapV3RouterAddress;
+    address public chainLinkETHUSDAddress;
 
-uint256 public ethPrice;
-uint256 public updatedPriceTime;
-uint256 public usdTargetPercentage = 40;
-uint256 public usdDividentPercentage = 25; // 25% of 40% = 10% Annual Drawdown
-uint256 private dividendFrequency = 5 minutes; // change to 1 years of production. 
-uint256 public nextDividendTS;
-address public owner;
+    uint256 public ethPrice;
+    uint256 public updatedPriceTime;
+    uint256 public usdTargetPercentage = 40;
+    uint256 public usdDividentPercentage = 25; // 25% of 40% = 10% Annual Drawdown
+    uint256 private dividendFrequency = 5 minutes; // change to 1 years of production.
+    uint256 public nextDividendTS;
+    address public owner;
 
-using SafeERC20 for IERC20;
-using SafeERC20 for DepositableERC20;
+    using SafeERC20 for IERC20;
+    using SafeERC20 for DepositableERC20;
 
+    IERC20 daiToken = IERC20(daiAddress);
+    DepositableERC20 wethToken = DepositableERC20(wethAddress);
+    IQuoter quoter = IQuoter(uniswapV3QuoterAddress);
+    IUniswapRouter uniswapRouter = IUniswapRouter(uniswapV3RouterAddress);
 
-constructor (
-address memory _daiAddress,
-address memory _wethAddress,
-address memory _uniswapV3QuoterAddress,
-address memory _uniswapV3RouterAddress,
-address memory _chainLinkETHUSDAddress
-) public {
-daiAddress = _daiAddress;
-wethAddress =_wethAddress;
-uniswapV3QuoterAddress = _uniswapV3QuoterAddress;
-uniswapV3RouterAddress = _uniswapV3RouterAddress;
-chainLinkETHUSDAddress = _chainLinkETHUSDAddress;
-console.log("Deploying myVault Version:", version);
-nextDividendTS = block.timestamp + dividendFrequency;
-owner = msg.sender;
-}
+    event myVaultLog(string msg, uint256 ref);
 
-IERC20 daiToken = IERC20(daiAddress);
-DepositableERC20 wethToken = DepositableERC20(wethAddress);
-IQuoter quoter = IQuoter(uniswapV3QuoterAddress);
-IUniswapRouter uniswapRouter = IUniswapRouter(uniswapV3RouterAddress);
-
-event myVaultLog(string msg, uint ref);
+    constructor(address[] memory contractAddress) {
+        daiAddress = contractAddress[0];
+        wethAddress = contractAddress[1];
+        uniswapV3QuoterAddress = contractAddress[2];
+        uniswapV3RouterAddress = contractAddress[3];
+        chainLinkETHUSDAddress = contractAddress[4];
+        console.log("Deploying myVault Version:", version);
+        nextDividendTS = block.timestamp + dividendFrequency;
+        owner = msg.sender;
+    }
 
 
+    function getDaiBalance() public view returns (uint256) {
+        return daiToken.balanceOf(address(this));
+    }
 
+    function getWethBalance() public view returns (uint256) {
+        return wethToken.balanceOf(address(this));
+    }
 
-function getDaiBalance() public view returns(uint256) {
-    return daiToken.balanceOf(address(this));
-}
+    function getTotalBalance() public view returns (uint256) {
+        require(ethPrice > 0, "Eth price has not been set");
+        require(
+            updatedPriceTime >= block.timestamp,
+            "Need to get updated Eth price"
+        );
+        uint256 daiBalance = getDaiBalance();
+        uint256 wethBalance = getWethBalance();
+        uint256 wethUSD = wethBalance * ethPrice;
+        uint256 totalBalance = wethUSD + daiBalance;
+        return totalBalance;
+    }
 
-function getWethBalance() public view returns(uint256){
-    return wethToken.balanceOf(address(this));
-}
+    function updateEthPriceUniswap() public returns (uint256) {
+        uint256 ethPriceRaw = quoter.quoteExactOutputSingle(
+            daiAddress,
+            wethAddress,
+            3000,
+            100000,
+            0
+        );
+        ethPrice = ethPriceRaw / 100000;
+        return ethPrice;
+    }
 
-function getTotalBalance() public view returns(uint256){
-    require(ethPrice > 0, "Eth price has not been set");
-    require(updatedPriceTime >= block.timestamp, "Need to get updated Eth price");
-    uint256 daiBalance = getDaiBalance();
-    uint256 wethBalance = getWethBalance();
-    uint256 wethUSD = wethBalance * ethPrice;
-    uint256 totalBalance = wethUSD + daiBalance;
-    return totalBalance;
-}
+    function updteEthPriceChainlink() public returns (uint256) {
+        int256 chainLinkEthPrice = EACAggregatorProxy(chainLinkETHUSDAddress)
+            .latestAnswer();
+        ethPrice = uint256(chainLinkEthPrice / 100000000);
+        return ethPrice;
+    }
 
-function updateEthPriceUniswap() public returns(uint256) {
-    uint256 ethPriceRaw = quoter.quoteExactOutputSingle(daiAddress, wethAddress, 3000, 100000,0);
-    ethPrice = ethPriceRaw / 100000;
-    return ethPrice;
-}
+    function buyWeth(uint256 amountUSD) internal {
+        uint256 deadline = block.timestamp + 15;
+        uint24 fee = 3000; //0.3% pool fee
+        address recipient = address(this);
+        uint256 amountIn = amountUSD; //includes 18 decimals
+        uint256 amountOutMinimum = 0; // if set to zero, front runners have an easier change of getting you
+        uint160 sqrtPriceLimitX96 = 0;
+        emit myVaultLog("AmountIn", amountIn);
+        require(
+            daiToken.approve(address(uniswapV3RouterAddress), amountIn),
+            "DAI approve failed"
+        );
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams(
+                daiAddress,
+                wethAddress,
+                fee,
+                recipient,
+                deadline,
+                amountIn,
+                amountOutMinimum,
+                sqrtPriceLimitX96
+            );
 
-function updteEthPriceChainlink() public returns(uint256) {
-    int256 chainLinkEthPrice = EACAggregatorProxy(chainLinkETHUSDAddress).latestAnswer();
-    ethPrice = uint256(chainLinkEthPrice / 100000000);
-    return ethPrice;
-}
+        uniswapRouter.exactInputSingle(params);
+        uniswapRouter.refundETH();
+    }
 
-function buyWeth(uint amountUSD) internal {
-    uint256 deadline = block.timestamp + 15;
-    uint24 fee = 3000; //pool fee
-    address recipient = address(this);
-    uint256 amountIn = amountUSD; //includes 18 decimals
-    uint256 amountOutMinimum = 0; // if set to zero, front runners have an easier change of getting you
-    uint160 sqrtPriceLimitX96 = 0;
-    emit myVaultLog("AmountIn", amountIn);
-    require(daiToken.approve(address(uniswapV3RouterAddress), amountIn), "DAI approve failed");
-    ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams(
-        daiAddress,
+    function sellWeth(uint256 amountUSD) internal {
+        uint256 deadline = block.timestamp + 15;
+        uint24 fee = 3000;
+        address recipient = address(this);
+        uint256 amountOut = amountUSD; //includes 18 decimals
+        uint256 amountInMaximum = 10 ** 28; 
+        uint160 sqrtPriceLimitX96 = 0;
+        require(wethToken.approve(address(uniswapV3RouterAddress), amountOut), "Weth Approve failed");
+        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter.ExactOutputSingleParams(
         wethAddress,
+        daiAddress,
         fee,
         recipient,
         deadline,
-        amountIn,
-        amountOutMinimum,
+        amountOut,
+        amountInMaximum,
         sqrtPriceLimitX96
-    );
+        );
 
-    uniswapRouter.exactInputSingle(params);
-    uniswapRouter.refundETH();
+        uniswapRouter.exactOutputSingle(params);
+        uniswapRouter.refundETH();
+    }
 
-}
+    function rebalance() public {
+        require(msg.sender == owner, "Only the owner can rebalance");
+        uint usdBalance = getDaiBalance();
+        uint totalBalance = getTotalBalance();
+        uint usdBalancePercentage = 100 * usdBalance / totalBalance;
+        emit myVaultLog("usdBalancePercentage: ", usdBalancePercentage);
+        if (usdBalancePercentage < usdTargetPercentage){
+            uint amountToSell = totalBalance / 100 * (usdTargetPercentage - usdBalancePercentage);
+            emit myVaultLog("Amount to Sell", amountToSell);
+            require (amountToSell > 0, "Nothing to sell");
+            sellWeth(amountToSell);
+        } else {
+            uint amountToBuy = totalBalance / 100 * (usdBalancePercentage - usdTargetPercentage);
+            emit myVaultLog("amountToBuy", amountToBuy);
+            require(amountToBuy > 0, "Nothing To Buy");
+            buyWeth(amountToBuy);
+        }
+        
+    }
 
+    // 10% annual dividend from strategy. 
+    function annualDividend() public {
+        require(msg.sender == owner, "You are not the owner");
+        require(block.timestamp > nextDividendTS, "Dividend is not yet due" );
+        uint balance = getDaiBalance();
+        uint amount = (balance * usdDividentPercentage) / 100;
+        daiToken.safeTransfer(owner, amount);
+        nextDividendTS = block.timestamp + dividendFrequency;
+    }
 
+    function closeAccount() public {
+        require(msg.sender == owner, "Only the owner can close thier account");
+        uint daiBalance = getDaiBalance();
+        if (daiBalance > 0) {
+            daiToken.safeTransfer(owner, daiBalance);
+        }
+        uint wethBalance = getWethBalance();
+        if (wethBalance > 0){
+            wethToken.safeTransfer(owner, wethBalance);
+        }
+    }
+
+    function recieve() public payable {
+        // accept ETH
+    }
+
+    function wrapETH() public {
+        require(msg.sender == owner, "Only the owner can conver ETH to WETH");
+        uint ethBalance = address(this).balance;
+        require(ethBalance > 0, "No ETH available to wrap");
+        emit myVaultLog("WrapETH:", ethBalance);
+        wethToken.deposit{value: ethBalance}();
+    }
 }
